532e413ead7f7fe6b3191bf1cf947e09fa1a00d9
Kaushan Ranasinghe <kaushan.gr@gmail.com>
2022-08-07 18:44:21 +0530

    Update Test_F_Master.v

--- Test_F_Master.v
+++ Test_F_Master.v
@@ -1,28 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   02:39:24 05/02/2022
-// Design Name:   Master
-// Module Name:   /home/ise/Bus_Architecture_2/Test_F_Master.v
-// Project Name:  Bus_Architecture_2
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: Master
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
-
 module Test_F_Master;
 
 	// Inputs
