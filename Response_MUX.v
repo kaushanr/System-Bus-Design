d51304e00c91aa119548515d687241ec4e6fca17
Kaushan Ranasinghe <kaushan.gr@gmail.com>
2022-08-07 18:32:38 +0530

    Update and rename Response MUX RTL code to Response_MUX.v

--- Response_MUX.v
+++ Response_MUX.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    03:09:28 04/24/2022 
-// Design Name: 
-// Module Name:    Resp_MUX 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module Resp_MUX(CLK,RST,HRESP,HRESP_1,HRESP_2,HRESP_3,SEL);
 
 input [1:0]SEL;
